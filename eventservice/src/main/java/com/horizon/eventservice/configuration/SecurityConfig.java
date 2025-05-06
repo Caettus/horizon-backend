0a2a4bf5bcd7a3139b604cbfdfd6a4e9b212b64d
Caettus <vincentscheffer2@gmail.com>
2025-05-06 23:27:32 +0200

    security toegevoegd, istg als dit niet werkt ga ik huilen

---
+++ eventservice/src/main/java/com/horizon/eventservice/configuration/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.horizon.eventservice.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(withDefaults())
+                );
+        return http.build();
+    }
+}
+
