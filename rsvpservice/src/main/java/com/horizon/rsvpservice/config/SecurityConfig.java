e1fbfb603aa52c76057fe73029a30d18989c6173
Caettus <vincentscheffer2@gmail.com>
2025-06-22 19:17:34 +0200

    dit werkt ook niet.

--- rsvpservice/src/main/java/com/horizon/rsvpservice/config/SecurityConfig.java
+++ rsvpservice/src/main/java/com/horizon/rsvpservice/config/SecurityConfig.java
@@ -1,21 +1,23 @@
-package com.horizon.eventservice.configuration;
+package com.horizon.rsvpservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Bean;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers(HttpMethod.GET, "/events").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/rsvps").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/public/**").permitAll()
                         .anyRequest().authenticated()
@@ -23,7 +25,6 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(withDefaults())
                 );
-        return http.build();
+        return httpSecurity.build();
     }
 }
-
